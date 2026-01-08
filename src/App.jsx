cf43d4dfc8871e226ab550824c3c6a56a10fb5d2
apna-project11 <01fe23bcs253@kletech.ac.in>
2026-01-08 22:12:40 +0530

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Skills from './components/Skills'
